5672a25852cf34bf73f6e97e72e8a218903596f8
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2024-03-04 10:28:50 -0500

    Adding Earbuds Scripts
    Adding the GET, POST, and PUT methods for Earbuds

---
+++ js/getEarBuds.js
@@ -0,0 +1,36 @@
+new gridjs.Grid({
+
+    search: true,
+    sort: true,
+    pagination: true,
+    fixedHeader: true,
+    height: "90%",
+
+    columns: [
+        { name: "id",  width: "100px" },
+        { name: "brand", width: "100px" },
+        { name: "model_number", width: "100px" },
+        { name: "wireless", width: "100px" },
+        { name: "noise_cancellation", width: "100px" } ],
+
+        server: {
+            url: "http://localhost:8007/api/v1/earbuds",
+            then: (data) =>
+            {
+                data.sort((a,b) => b.id - a.id);
+                data.sort((c,d) => c.brand - d.brand);
+
+                return data.map((earbuds) => [
+                    earbuds.id,
+                    earbuds.brand,
+                    earbuds.model_number,
+                    earbuds.wireless,
+                    earbuds.noise_cancellation
+                ]);
+            } 
+            
+        },
+    }).render(document.getElementById("table"));
+
+    
+   
